59fc280c3015aeacd739634679aea28f726977f1
Michael Siebenmann <msiebenmann@student.ethz.ch>
2024-11-06 22:58:22 +0100

    fix time units when creating plots

--- scripts/plotting.py
+++ scripts/plotting.py
@@ -39,8 +39,8 @@ def compute_timing_metrics(timings: List[int]) -> Tuple[float, int, int, float]:
         print("Plotting error: empty timings")
         exit(1)
 
-    # Convert timings from nanoseconds to milliseconds
-    timings_ms = np.array(timings) / 1e6
+    # Convert timings from nanoseconds to seconds
+    timings_ms = np.array(timings) / 1e9
 
     # Calculate  metrics
     median_timings = np.median(timings_ms)
