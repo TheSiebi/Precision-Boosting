82c766f4e515e0d6991d4161c1796ec678a3b56d
Michael Mörschelll <mmorsche@gmail.com>
2024-11-13 17:23:29 +0100

    Basic Ozaki CPU implementation

--- src/matmul.h
+++ src/matmul.h
@@ -23,4 +23,7 @@ void matmul_Oootomo_v1(float *A, float *B, float *C, int M, int K, int N);
 void matmul_cuBLAS32(float *A, float *B, float *C, int M, int K, int N);
 void matmul_cuBLAS64(double *A, double *B, double *C, int M, int K, int N);
 
+// Ozaki paper uses A [m, n] and B [n, p] matrices
+void matmul_Ozaki_v0(double *a, double *b, double *c, int m, int n, int p);
+
 #endif // MATMUL_H
