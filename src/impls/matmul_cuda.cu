7ddc4d4d48ba8142bad86136ab5642ca1ccea51d
Marc <matter.marc@outlook.com>
2024-12-05 17:07:46 +0100

    make type generic

--- src/impls/matmul_cuda.cu
+++ src/impls/matmul_cuda.cu
@@ -6,6 +6,7 @@
 #include <cuda_fp16.h>
 #include <mma.h>
 #include <driver_types.h>
+#include <type_traits>
 
 #include "../matmul.h"
 #include "../profiler.h"
@@ -246,14 +247,21 @@ __global__ void matmul_kernel_v4(InputType *A, InputType *B, OutputType *C, int 
             wmma::store_matrix_sync(C + offsetC + (m * FragSizeM * N + n * FragSizeN) , cFrag[m][n], N, wmma::mem_row_major);
 }
 
-
-template <const int BM, const int BN, const int BK>
-__device__ void loadFromGmem(int N, int K, const float *A, const float *B, 
-    float *As, float *Bs, int innerRowA, int innerColA, int rowStrideA, int innerRowB, int innerColB, int rowStrideB)
+struct alignas(8) half4 
 {
+    half x, y, z, w;
+};
+
+template <const int BM, const int BN, const int BK, typename T>
+__device__ void loadFromGmem(int N, int K, const T *A, const T *B, 
+    T *As, T *Bs, int innerRowA, int innerColA, int rowStrideA, int innerRowB, int innerColB, int rowStrideB)
+{
+    using vecT = typename std::conditional<std::is_same<T, half>::value, half4, 
+                    typename std::conditional<std::is_same<T, float>::value, float4, double4>::type>::type;
+
     for (int offset = 0; offset + rowStrideA <= BM; offset += rowStrideA)
     {
-        const float4 tmp =  reinterpret_cast<const float4 *>(
+        const vecT tmp =  reinterpret_cast<const vecT *>(
             &A[(innerRowA + offset) * K + innerColA * 4])[0];
         
         // load A in ColMajor order
@@ -265,18 +273,19 @@ __device__ void loadFromGmem(int N, int K, const float *A, const float *B,
     
     for (int offset = 0; offset + rowStrideB <= BK; offset += rowStrideB)
     {
-        reinterpret_cast<float4 *>(
+        reinterpret_cast<vecT *>(
             &Bs[(innerRowB + offset) * BN + innerColB * 4])[0] =
-        reinterpret_cast<const float4 *>(
+        reinterpret_cast<const vecT *>(
             &B[(innerRowB + offset) * N + innerColB * 4])[0];
     }
 }
 
 template <const int BM, const int BN, const int BK, const int WM, const int WN, 
     const int WMITER, const int WNITER, const int WSUBM, const int WSUBN, 
-    const int TM, const int TN>
-__device__ void warpMatmul(float *regM, float *regN, float *threadResults, const float *As,
-    const float *Bs, const int warpRow, const int warpCol, const int threadRowInWarp, const int threadColInWarp)
+    const int TM, const int TN,
+    typename InputType, typename OutputType>
+__device__ void warpMatmul(OutputType *regM, OutputType *regN, OutputType *threadResults, const InputType *As,
+    const InputType *Bs, const int warpRow, const int warpCol, const int threadRowInWarp, const int threadColInWarp)
 {
     // Compute results of warp in an outer product instead of an inner product for better cache reuse
     for (int dotIdx = 0; dotIdx < BK; dotIdx++)
@@ -287,7 +296,7 @@ __device__ void warpMatmul(float *regM, float *regN, float *threadResults, const
             for (int i = 0; i < TM; i++)
             {
                 regM[wSubRowIdx * TM + i] = 
-                    As[dotIdx * BM + warpRow * WM + wSubRowIdx * WSUBM + threadRowInWarp * TM + i];
+                    (OutputType) As[dotIdx * BM + warpRow * WM + wSubRowIdx * WSUBM + threadRowInWarp * TM + i];
             }
         }
 
@@ -296,7 +305,7 @@ __device__ void warpMatmul(float *regM, float *regN, float *threadResults, const
             for (int i = 0; i < TN; i++)
             {
                 regN[wSubColIdx * TN + i] = 
-                    Bs[dotIdx * BN + warpCol * WN + wSubColIdx * WSUBN + threadColInWarp * TN + i];
+                    (OutputType) Bs[dotIdx * BN + warpCol * WN + wSubColIdx * WSUBN + threadColInWarp * TN + i];
             }
         }
 
@@ -332,8 +341,9 @@ __device__ void warpMatmul(float *regM, float *regN, float *threadResults, const
  * - TN * TM % 4 == 0
  */
 template <const int BM, const int BN, const int BK, const int WM, const int WN, 
-          const int WMITER, const int WNITER, const int TM, const int TN>
-__global__ void matmul_kernel_float_v0(const float *A, const float *B, float *C, int M, int K, int N)
+          const int WMITER, const int WNITER, const int TM, const int TN,
+          typename InputType, typename OutputType>
+__global__ void matmul_kernel_float_v0(const InputType *A, const InputType *B, OutputType *C, int M, int K, int N)
 {
     const int cRow = blockIdx.x;
     const int cCol = blockIdx.y;
@@ -354,8 +364,8 @@ __global__ void matmul_kernel_float_v0(const float *A, const float *B, float *C,
     const int threadColInWarp = warpLane % (WSUBN / TN);
 
     // allocate space for the current blocktile in shared memory
-    __shared__ float As[BM * BK];
-    __shared__ float Bs[BK * BN];
+    __shared__ InputType As[BM * BK];
+    __shared__ InputType Bs[BK * BN];
 
     // Move blocktile to beggining of A's row and B's column
     A += cRow * BM * K;
@@ -377,28 +387,31 @@ __global__ void matmul_kernel_float_v0(const float *A, const float *B, float *C,
     const int rowStrideB = elemsPerThread * blockDim.x / BN;
 
     // thread-local cache for results in registerfile
-    float threadResults[WMITER * TM * WNITER * TN] = {0.0f};
+    OutputType threadResults[WMITER * TM * WNITER * TN] = {0.0};
     // thread-local cache for A and B
-    float regM[WMITER * TM] = {0.0f};
-    float regN[WNITER * TN] = {0.0f};
+    OutputType regM[WMITER * TM] = {0.0};
+    OutputType regN[WNITER * TN] = {0.0};
 
     // Loop over all block tiles
     for (int bkIdx = 0; bkIdx < K; bkIdx += BK)
     {
         // populate SMEM cache
-        loadFromGmem<BM, BN, BK>(N, K, A, B, As, Bs, innerRowA, innerColA, rowStrideA,
+        loadFromGmem<BM, BN, BK, InputType>(N, K, A, B, As, Bs, innerRowA, innerColA, rowStrideA,
             innerRowB, innerColB, rowStrideB); 
 
         __syncthreads();
 
-        warpMatmul<BM, BN, BK, WM, WN, WMITER, WNITER, WSUBM, WSUBN, TM, TN>(regM, regN,
-            threadResults, As, Bs, warpRow, warpCol, threadRowInWarp, threadColInWarp);
+        warpMatmul<BM, BN, BK, WM, WN, WMITER, WNITER, WSUBM, WSUBN, TM, TN, InputType, OutputType>
+            (regM, regN, threadResults, As, Bs, warpRow, warpCol, threadRowInWarp, threadColInWarp);
 
         // advance blocktile
         A += BK;
         B += BK * N;
         __syncthreads();
     }
+
+    using vecT = typename std::conditional<std::is_same<OutputType, half>::value, half4, 
+                    typename std::conditional<std::is_same<OutputType, float>::value, float4, double4>::type>::type;
 
     // Store results back to C matrix
     for (int wSubRowIdx = 0; wSubRowIdx < WMITER; wSubRowIdx++)
@@ -412,10 +425,10 @@ __global__ void matmul_kernel_float_v0(const float *A, const float *B, float *C,
                 {
                     int i = (wSubRowIdx * TM + resIdxM) * (WNITER * TN) + 
                         wSubColIdx * TN + resIdxN;
-                    float4 tmp = {threadResults[i], threadResults[i + 1], threadResults[i + 2], threadResults[i + 3]};
+                    vecT tmp = {threadResults[i], threadResults[i + 1], threadResults[i + 2], threadResults[i + 3]};
 
                     // vectorized store to GMEM
-                    reinterpret_cast<float4 *>(
+                    reinterpret_cast<vecT *>(
                         &C[C_Offset + (threadRowInWarp * TM + resIdxM) * N + 
                            threadColInWarp * TN + resIdxN])[0] = tmp;
                 }
@@ -522,7 +535,7 @@ void matmul_float(InputType *A, InputType *B, OutputType *C, int M, int K, int N
         dim3 blocks(M / p.BM, N / p.BN);
         if constexpr(version == 0)
         {
-            matmul_kernel_float_v0<p.BM, p.BN, p.BK, p.WM, p.WN, p.WMITER, p.WNITER, p.TM, p.TN>
+            matmul_kernel_float_v0<p.BM, p.BN, p.BK, p.WM, p.WN, p.WMITER, p.WNITER, p.TM, p.TN, InputType, OutputType>
                 <<<blocks, p.threadsPerBlock>>>(A, B, C, M, K, N);
         }
     } 
@@ -536,7 +549,7 @@ void matmul_float(InputType *A, InputType *B, OutputType *C, int M, int K, int N
         dim3 blocks(M / p.BM, N / p.BN);
         if constexpr(version == 0)
         {
-            matmul_kernel_float_v0<p.BM, p.BN, p.BK, p.WM, p.WN, p.WMITER, p.WNITER, p.TM, p.TN>
+            matmul_kernel_float_v0<p.BM, p.BN, p.BK, p.WM, p.WN, p.WMITER, p.WNITER, p.TM, p.TN, InputType, OutputType>
                 <<<blocks, p.threadsPerBlock>>>(A, B, C, M, K, N);
         }
     }
