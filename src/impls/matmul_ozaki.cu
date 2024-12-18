368da1c7d30a3f49c6650fcb16088a14c574d1e8
Max Striebel <max.striebel@gmail.com>
2024-12-18 17:45:28 +0100

    Start adding optimized ozaki version

--- src/impls/matmul_ozaki.cu
+++ src/impls/matmul_ozaki.cu
@@ -216,6 +216,35 @@ std::vector<std::vector<float>> ozaki_split_to_float(const size_t m, const size_
 
 }
 
+template<int splitCount>
+void ozaki_split_to_float_fixed(double* A, float* ASplit, const size_t M, const size_t N)
+{
+    // beta = fl(...)
+    const float beta = ceilf((log2f(N) + 24) / 2.f);
+
+    for(int k = 0; k < splitCount-1; k++)
+    {
+        for(size_t i = 0; i < M; i++)
+        {
+            double mu = 0;
+            for(size_t j = 0; j < N; j++)
+                mu = std::max(mu, std::abs(A[ix(i, j, M, N)]));
+            float w = exp2f(ceilf(log2f(mu)) + beta);
+
+            for(size_t j = 0; j < N; j++)
+            {
+                size_t ij = ix(i, j, M, N);
+                float value = (float)(A[ij] + (double)w) - w;
+                ASplit[k*M*N + ij] = value;
+                A[ij] -= (double)value;
+            }
+        }
+    }
+
+    for (size_t ij = 0; ij < M * N; ++ij)
+        ASplit[(splitCount-1)*M*N + ij] = (float) A[ij];
+}
+
 
 /**
  * Implementation of Algorithm 4 in Ozaki paper.
@@ -230,7 +259,7 @@ std::vector<std::vector<float>> ozaki_mul(const size_t m, const size_t n, const 
 
     if constexpr (version == 0 || version == 1)
     {
-        PROFILE_FUNCTION_SEGMENT_START("split");
+        PROFILE_SEGMENT_START("split");
         // D = Split_Mat(A, inf, delta); nA = length(D);
         auto D = ozaki_split_to_float(m, n, a, MAX_SPLITS);
         const auto nA = D.size();
@@ -242,12 +271,12 @@ std::vector<std::vector<float>> ozaki_mul(const size_t m, const size_t n, const 
         auto E = ozaki_split_to_float(n, p, b, MAX_SPLITS);
         const auto nB = E.size();
         *nB_ptr = (int) nB;
-        PROFILE_SEGMENT_FUNCTION_END();
 
         // for r = 1 : nB, E{r} = E{r}T ; end
         // again, why transpose?
         // for (auto& matrix: E)
         //     transpose(p, n, matrix.data());
+        PROFILE_SEGMENTS_SWITCH("matmul");
 
         int t = 0;
         std::vector<std::vector<float>> C(nA * nB, std::vector<float>(m * p));
@@ -263,7 +292,7 @@ std::vector<std::vector<float>> ozaki_mul(const size_t m, const size_t n, const 
                     throw std::runtime_error(std::string("unimplemented ozaki version " + version));
             }
         }
-
+        PROFILE_SEGMENT_END();
         return C;
     }
 
@@ -287,7 +316,6 @@ std::vector<std::vector<float>> ozaki_mul(const size_t m, const size_t n, const 
 
         return C;
     }
-
     else
         throw std::runtime_error(std::string("unimplemented ozaki version " + version));
 
@@ -297,6 +325,7 @@ std::vector<std::vector<float>> ozaki_mul(const size_t m, const size_t n, const 
 template<int version>
 flop_counts matmul_ozaki(double *a, double *b, double *c, size_t m, size_t n, size_t p)
 {
+    PROFILE_FUNCTION_START();
     // Ozaki splitting modifies input matrices. Therefore, copies must be made.
     std::vector<double> a_copy(a, a + m * n);
     std::vector<double> b_copy(b, b + n * p);
@@ -304,18 +333,16 @@ flop_counts matmul_ozaki(double *a, double *b, double *c, size_t m, size_t n, si
     // Splitting configuration (nA, nB) influences flop-count, and must be retrieved.
     int64_t nA, nB;
 
-    PROFILE_FUNCTION_START();
     const auto unevaluated_sum = ozaki_mul<version>(m, n, p, a_copy.data(), b_copy.data(), &nA, &nB);
     // std::cout << "nA: " << nA << "/" << MAX_SPLITS << ", ";
     // std::cout << "nB: " << nB << "/" << MAX_SPLITS << "\n";
-    PROFILE_FUNCTION_SEGMENT_START("accumulate");
+    PROFILE_SEGMENT_START("accumulate");
     memset(c, 0, m * p * sizeof(double));
     for (const auto& matrix: unevaluated_sum)
         for (size_t ij = 0; ij < m * p; ++ij)
             c[ij] += matrix[ij];
+    
     PROFILE_SEGMENT_FUNCTION_END();
-
-    PROFILE_FUNCTION_END();
 
     flop_counts counts =
     {
@@ -325,7 +352,63 @@ flop_counts matmul_ozaki(double *a, double *b, double *c, size_t m, size_t n, si
     };
     return counts;
 }
-
 template flop_counts matmul_ozaki<0>(double *a, double *b, double *c, size_t m, size_t n, size_t p);
 template flop_counts matmul_ozaki<1>(double *a, double *b, double *c, size_t m, size_t n, size_t p);
 template flop_counts matmul_ozaki<2>(double *a, double *b, double *c, size_t m, size_t n, size_t p);
+
+template<int version>
+flop_counts matmul_ozaki_optimized(double *a, double *b, double *c, size_t M, size_t K, size_t N)
+{
+    PROFILE_FUNCTION_START();
+    // Ozaki splitting modifies input matrices. Therefore, copies must be made.
+    std::vector<double> a_copy(a, a + M * K);
+    std::vector<double> b_copy(b, b + K * N);
+
+    // Splitting configuration (nA, nB) influences flop-count, and must be retrieved.
+
+    PROFILE_SEGMENT_START("split");
+
+    constexpr int splitCount = MAX_SPLITS;
+    constexpr int mulCount = splitCount * splitCount;
+
+    float *ASplit = (float*)malloc(splitCount * M * K * sizeof(float));
+    float *BSplit = (float*)malloc(splitCount * K * N * sizeof(float));
+    float *CSplit = (float*)malloc(mulCount * M * N * sizeof(float));
+
+    ozaki_split_to_float_fixed<splitCount>(a_copy.data(), ASplit, M, K);
+    ozaki_split_to_float_fixed<splitCount>(b_copy.data(), BSplit, K, N);
+
+    PROFILE_SEGMENTS_SWITCH("matmul");
+
+    int t = 0;
+    std::vector<std::vector<float>> C(mulCount, std::vector<float>(M * N));
+    for (int r = 0; r < splitCount; ++r)
+    {
+        for (int s = 0; s < splitCount; ++s)
+        {
+            matmul_cuda<float, float, 1, false>(&ASplit[r*M*K], &BSplit[s*K*N], &CSplit[t*M*N], M, K, N);
+            t++;
+        }
+    }
+
+    PROFILE_SEGMENTS_SWITCH("accumulate");
+
+    memset(c, 0, M * N * sizeof(double));
+    for(int i = 0; i < mulCount; i++)
+        for (size_t ij = 0; ij < M * N; ++ij)
+            c[ij] += CSplit[i*M*N + ij];
+    
+    PROFILE_SEGMENT_FUNCTION_END();
+
+    int64_t nA = splitCount;
+    int64_t nB = splitCount;
+    flop_counts counts =
+    {
+        0L,
+        8L + (4L*M+3L*M*K)*nA + (4L*K+3L*K*N)*nB + 2L*nA*nB*M*K*N,
+        (2L*M*K+M)*nA + (2L*K*N+K)*nB + M*N*nA*nB
+    };
+    return counts;
+}
+
+template flop_counts matmul_ozaki_optimized<0>(double *a, double *b, double *c, size_t M, size_t K, size_t N);
